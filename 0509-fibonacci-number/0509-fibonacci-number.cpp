b6ebcdcb5788110b9ce808ded1a8377d15226614
Kaushik Dugar <deerex1211@gmail.com>
2026-05-07 23:57:35 +0530

    0509-fibonacci-number 3 ms (62.72%) 7.5 MB (98.66%) Easy

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        int ft = 0, st = 1;
+        for(int i = 1; i <= n; i++){
+            int tt = ft + st;
+            ft = st;
+            st = tt;
+        }
+        return ft;
+    }
+};
