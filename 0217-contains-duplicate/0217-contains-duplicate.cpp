8b5ca939a06e177f2d2d9cb4c8d81084de4bb12e
Kaushik Dugar <deerex1211@gmail.com>
2026-05-12 23:55:42 +0530

    Time: 55 ms (31.56%), Space:90.9 MB (27.42%)

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
-        unordered_map<int, int> checked;
-        for(int num:nums){
-            if(checked[num]>=1)
-            return true;
-            checked[num]++;
+        unordered_map<int, int> visited;
+        for (int num : nums) {
+            if (visited[num] >= 1)
+                return true;
+            visited[num]++;
         }
         return false;
     }
