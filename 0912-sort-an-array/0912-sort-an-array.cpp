542e6ba2844d7c97c51793b25f5a9effed5bb287
Kaushik Dugar <deerex1211@gmail.com>
2026-05-20 14:32:17 +0530

    Time: 26 ms (83.21%), Space:70.8 MB (93.13%)

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,28 +1,8 @@
 class Solution {
 public:
-    void heapify(vector<int>& a, int n, int i) {
-        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
-
-        if (l < n && a[l] > a[largest]) largest = l;
-        if (r < n && a[r] > a[largest]) largest = r;
-
-        if (largest != i) {
-            swap(a[i], a[largest]);
-            heapify(a, n, largest);
-        }
-    }
-
     vector<int> sortArray(vector<int>& nums) {
-        int n = nums.size();
-
-        for (int i = n / 2 - 1; i >= 0; --i)
-            heapify(nums, n, i);
-
-        for (int i = n - 1; i > 0; --i) {
-            swap(nums[0], nums[i]);
-            heapify(nums, i, 0);
-        }
-
+        sort(nums.begin(),nums.end());
         return nums;
+        
     }
 };
