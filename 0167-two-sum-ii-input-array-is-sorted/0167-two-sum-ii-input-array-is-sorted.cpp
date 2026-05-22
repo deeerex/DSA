8efd9494d2d888d8f2634fee8d2833615fb19306
Kaushik Dugar <deerex1211@gmail.com>
2026-05-22 19:54:25 +0530

    Time: 0 ms (100%), Space:19.5 MB (32.8%)

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& numbers, int target) {
-        int start = 0;
-        int end = numbers.size() - 1;
+        int start = 0,end = numbers.size() - 1;
         while(start < end){
             int sum = numbers[start] + numbers[end];
-            if(sum == target){
-                return {start + 1, end + 1};
-            }
-            else if(sum < target){
-                start++;
-            }
-            else{
-                end--;
-            }
+            if(sum == target) return {start + 1, end + 1};
+            else if(sum < target) start++;
+            else end--;
         }
         return {-1, -1};
     }
