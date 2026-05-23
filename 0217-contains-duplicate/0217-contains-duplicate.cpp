86ab2ec2ac2b3c895d5ede72097c183cc4cec4c7
Kaushik Dugar <deerex1211@gmail.com>
2026-05-23 23:37:42 +0530

    Time: 53 ms (37.16%), Space:90.7 MB (61.02%)

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        unordered_set<int> seen;
-        for (int num : nums) {
-            if (seen.find(num) != seen.end()) {
-                return true;  
-            }
-            seen.insert(num);
+   bool containsDuplicate(vector<int>& nums) {
+    unordered_set<int> seen;          // empty set to track visited numbers
+
+    for (int num : nums) {            // iterate through each number
+        if (seen.count(num)) {        // if num already in set → duplicate!
+            return true;
         }
-        return false;   
+        seen.insert(num);             // otherwise, mark it as seen
     }
+
+    return false;                     // no duplicates found
+}
 };
+
